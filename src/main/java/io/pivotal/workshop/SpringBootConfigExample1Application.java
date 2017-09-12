b59e381dba4aab44181fc799044a1669bdf84851
rejivp <30458685+rejivp@users.noreply.github.com>
2017-09-12 15:56:47 +0530

    Second commit

--- src/main/java/io/pivotal/workshop/SpringBootConfigExample1Application.java
+++ src/main/java/io/pivotal/workshop/SpringBootConfigExample1Application.java
@@ -3,6 +3,7 @@ package io.pivotal.workshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringBootConfigExample1Application {
 
